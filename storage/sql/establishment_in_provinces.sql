be2f5377ed4d81af09bccd84c58ed4eff62207a9
Saac Sornchai <sornchai.la@ku.th>
2022-01-25 13:42:31 +0700

    view add tsic

--- storage/sql/establishment_in_provinces.sql
+++ storage/sql/establishment_in_provinces.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW establishments_in_provinces AS
-SELECT Province.`name` AS `province_name`, Region.`name` AS `region_name`, COUNT(DISTINCT(Es.id)) AS establishment_count
+SELECT Province.`name` AS `province_name`, Region.`name` AS `region_name`, Es.tsic_code,
+       COUNT(DISTINCT(Es.id)) AS establishment_count
 FROM `establishments` AS Es
          JOIN `address_establishment` AS AddrEs ON (Es.id = AddrEs.establishment_id)
          JOIN `addresses` AS Address ON (Address.id = AddrEs.address_id)
@@ -8,10 +9,10 @@ FROM `establishments` AS Es
          JOIN `provinces` AS Province ON (Province.id = District.province_id)
          JOIN `regions` AS Region ON (Region.id = Province.region_id)
 
-GROUP BY Province.`name`, Region.`name`;
+GROUP BY Province.`name`, Region.`name`, Es.tsic_code;
 
 CREATE OR REPLACE VIEW work_forces_in_provinces AS
-SELECT Province.`name` AS `province_name`, Region.`name` AS `region_name`,
+SELECT Province.`name` AS `province_name`, Region.`name` AS `region_name`, Es.tsic_code,
        SUM(WorkForce.no_person_engaged) AS `sum_person_engaged`,
        SUM(WorkForce.no_employee) AS `sum_employee`
 FROM `establishments` AS Es
@@ -23,10 +24,10 @@ FROM `establishments` AS Es
          JOIN `regions` AS Region ON (Region.id = Province.region_id)
          JOIN `work_force_employees` AS WorkForce ON (WorkForce.establishment_id = Es.id)
 
-GROUP BY Province.`name`, Region.`name`;
+GROUP BY Province.`name`, Region.`name`, Es.tsic_code;
 
 CREATE OR REPLACE VIEW financial_statements_in_provinces AS
-SELECT Province.`name` AS `province_name`, Region.`name` AS `region_name`,
+SELECT Province.`name` AS `province_name`, Region.`name` AS `region_name`, Es.tsic_code,
        SUM(Finance.registered_capital) AS `sum_registered_capital`,
        SUM(Finance.total_income) AS `sum_total_income`,
        SUM(Finance.net_profit_loss) AS `sum_net_profit_loss`,
@@ -41,4 +42,4 @@ FROM `establishments` AS Es
          JOIN `regions` AS Region ON (Region.id = Province.region_id)
          JOIN `financial_statements` AS Finance ON (Finance.establishment_id = Es.id)
 
-GROUP BY Province.`name`, Region.`name`;
+GROUP BY Province.`name`, Region.`name`, Es.tsic_code;
