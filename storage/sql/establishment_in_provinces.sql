619b97cda69cdea2df1d9b69b5671499ea8157c7
Saac Sornchai <sornchai.la@ku.th>
2022-01-25 13:23:54 +0700

    add more views

--- storage/sql/establishment_in_provinces.sql
+++ storage/sql/establishment_in_provinces.sql
@@ -9,3 +9,36 @@ FROM `establishments` AS Es
          JOIN `regions` AS Region ON (Region.id = Province.region_id)
 
 GROUP BY Province.`name`, Region.`name`;
+
+CREATE OR REPLACE VIEW work_forces_in_provinces AS
+SELECT Province.`name` AS `province_name`, Region.`name` AS `region_name`,
+       SUM(WorkForce.no_person_engaged) AS `sum_person_engaged`,
+       SUM(WorkForce.no_employee) AS `sum_employee`
+FROM `establishments` AS Es
+         JOIN `address_establishment` AS AddrEs ON (Es.id = AddrEs.establishment_id)
+         JOIN `addresses` AS Address ON (Address.id = AddrEs.address_id)
+         JOIN `subdistricts` AS SubDistrict ON (SubDistrict.id = Address.subdistrict_id)
+         JOIN `districts` AS District ON (District.id = SubDistrict.district_id)
+         JOIN `provinces` AS Province ON (Province.id = District.province_id)
+         JOIN `regions` AS Region ON (Region.id = Province.region_id)
+         JOIN `work_force_employees` AS WorkForce ON (WorkForce.establishment_id = Es.id)
+
+GROUP BY Province.`name`, Region.`name`;
+
+CREATE OR REPLACE VIEW financial_statements_in_provinces AS
+SELECT Province.`name` AS `province_name`, Region.`name` AS `region_name`,
+       SUM(Finance.registered_capital) AS `sum_registered_capital`,
+       SUM(Finance.total_income) AS `sum_total_income`,
+       SUM(Finance.net_profit_loss) AS `sum_net_profit_loss`,
+       SUM(Finance.total_assets) AS `sum_total_assets`
+
+FROM `establishments` AS Es
+         JOIN `address_establishment` AS AddrEs ON (Es.id = AddrEs.establishment_id)
+         JOIN `addresses` AS Address ON (Address.id = AddrEs.address_id)
+         JOIN `subdistricts` AS SubDistrict ON (SubDistrict.id = Address.subdistrict_id)
+         JOIN `districts` AS District ON (District.id = SubDistrict.district_id)
+         JOIN `provinces` AS Province ON (Province.id = District.province_id)
+         JOIN `regions` AS Region ON (Region.id = Province.region_id)
+         JOIN `financial_statements` AS Finance ON (Finance.establishment_id = Es.id)
+
+GROUP BY Province.`name`, Region.`name`;
